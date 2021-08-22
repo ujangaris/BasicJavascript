bdd2305b4eb00a2393a72fb2e8947b9ff34f2dca
ujangaja <ujangaja@gmail.com>
2021-08-22 08:54:16 +0700

    #3 DOM Selection | menyeleksi untuk manipulasi menggunakan getElementById

---
+++ javascriptDOM/domSelection/script.js
@@ -0,0 +1,11 @@
+// DOM Selection
+// document.getElementById()
+// javascript tolon carikan saya id => mengembalikan element
+const judul = document.getElementById("judul");
+
+//merubah warna tulisan
+judul.style.color = "red";
+//merubah warna backgroundColor
+judul.style.backgroundColor = "#ccc";
+//merubah tulisannya
+judul.innerHTML = "Ujang Arisandi";
