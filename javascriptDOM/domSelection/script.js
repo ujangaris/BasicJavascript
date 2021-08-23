57c9d99b11caaeede78b92e4345392df661ce79b
ujangaja <ujangaja@gmail.com>
2021-08-23 14:14:55 +0700

    Javascript & DOM #4 - DOM Selection (Part 2)

--- javascriptDOM/domSelection/script.js
+++ javascriptDOM/domSelection/script.js
@@ -1,4 +1,4 @@
-// DOM Selection
+/* // DOM Selection
 // document.getElementById()
 // javascript tolon carikan saya id => mengembalikan element
 const judul = document.getElementById("judul");
@@ -26,4 +26,23 @@ h1.style.fontSize = "50px";
 // document.getElementByClassName()
 //=> mengembalikan HTMLCollection
 const p1 = document.getElementsByClassName("p1");
-p1[0].innerHTML = 'ini dirubah dari javascript'
+p1[0].innerHTML = "ini dirubah dari javascript"; */
+
+//document.querySelector()
+
+const p4 = document.querySelector("#b p");
+p4.style.color = "green";
+p4.fontSize = "30px";
+
+const li2 = document.querySelector("section#b ul li:nth-child(2)");
+li2.style.backgroundColor = "orange";
+
+// const p = document.querySelector("p");//ini hanya merubah p pertama
+// p.innerHTML = "Ini dirubah melalui javascript loh";
+
+//jika ingin merubah semua p makan gunakan querySelectorAll()
+// document.querySelectorAll()
+const p = document.querySelectorAll("p");
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "pink";
+}
